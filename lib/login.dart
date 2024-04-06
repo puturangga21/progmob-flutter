d0abc31a366e6e03ccdc793a5efe9b4a84d6fd84
puturangga21 <puturangga21@gmail.com>
2024-04-06 17:38:04 +0800

    fix: Refactor register.dart to add SingleChildScrollView and SafeArea

--- lib/login.dart
+++ lib/login.dart
@@ -12,170 +12,172 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 70, left: 33, right: 33),
-            child: Column(
-              children: [
-                // Icon
-                SvgPicture.asset('assets/icons/mini_icons.svg'),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 70, left: 33, right: 33),
+              child: Column(
+                children: [
+                  // Icon
+                  SvgPicture.asset('assets/icons/mini_icons.svg'),
 
-                const SizedBox(height: 36),
+                  const SizedBox(height: 36),
 
-                // Heading
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Text(
-                    'Sudah Daftar? Silahkan Login',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w600,
-                      color: secondary,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                Text(
-                  'Kami menjamin keamanan dan privasi anda',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    color: neutral,
-                    fontSize: 12,
-                  ),
-                ),
-
-                const SizedBox(height: 50),
-
-                // Form
-                const Column(
-                  children: [
-                    // Username
-                    MyTextField(
-                      hintText: 'Username',
-                      title: 'Username',
-                      myIcons: 'assets/icons/user.svg',
-                      hideText: false,
-                    ),
-
-                    SizedBox(height: 20),
-
-                    // Password
-                    MyTextField(
-                      hintText: 'Password',
-                      title: 'Password',
-                      myIcons: 'assets/icons/key.svg',
-                      hideText: true,
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 15),
-
-                // Lupa password?
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Lupa Password?',
+                  // Heading
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Text(
+                      'Sudah Daftar? Silahkan Login',
                       style: TextStyle(
+                        fontSize: 30,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w600,
                         color: secondary,
-                        fontSize: 14,
                       ),
-                    )
-                  ],
-                ),
-
-                const SizedBox(height: 15),
-
-                // Tombol
-                MyButton(
-                  color: primary,
-                  title: 'Login',
-                ),
-
-                // Atau login dengan
-                const SizedBox(height: 50),
-
-                Row(
-                  children: [
-                    // Garis hiasan
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: neutral,
-                      ),
+                      textAlign: TextAlign.center,
                     ),
+                  ),
 
-                    // Teks
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Atau login dengan',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          color: neutral,
-                        ),
-                      ),
-                    ),
+                  const SizedBox(height: 10),
 
-                    // Garis hiasan
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: neutral,
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 40),
-
-                const Row(
-                  children: [
-                    // Card login untuk facebook
-                    CardLoginSocial(
-                      logo: 'assets/icons/facebook.svg',
-                      title: 'Facebook',
-                    ),
-
-                    SizedBox(width: 15),
-
-                    // Card login untuk google
-                    CardLoginSocial(
-                      logo: 'assets/icons/google.svg',
-                      title: 'Google',
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 110),
-
-                RichText(
-                  text: TextSpan(
-                    text: 'Belum punya akun? ',
+                  Text(
+                    'Kami menjamin keamanan dan privasi anda',
                     style: TextStyle(
                       fontFamily: 'Poppins',
-                      fontSize: 14,
-                      color: secondary,
+                      color: neutral,
+                      fontSize: 12,
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Daftar',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, color: primary),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // Form
+                  const Column(
+                    children: [
+                      // Username
+                      MyTextField(
+                        hintText: 'Username',
+                        title: 'Username',
+                        myIcons: 'assets/icons/user.svg',
+                        hideText: false,
+                      ),
+
+                      SizedBox(height: 20),
+
+                      // Password
+                      MyTextField(
+                        hintText: 'Password',
+                        title: 'Password',
+                        myIcons: 'assets/icons/key.svg',
+                        hideText: true,
                       ),
                     ],
                   ),
-                )
-              ],
+
+                  const SizedBox(height: 15),
+
+                  // Lupa password?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Lupa Password?',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          color: secondary,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  // Tombol
+                  MyButton(
+                    color: primary,
+                    title: 'Login',
+                  ),
+
+                  // Atau login dengan
+                  const SizedBox(height: 50),
+
+                  Row(
+                    children: [
+                      // Garis hiasan
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: neutral,
+                        ),
+                      ),
+
+                      // Teks
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Atau login dengan',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: neutral,
+                          ),
+                        ),
+                      ),
+
+                      // Garis hiasan
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: neutral,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 40),
+
+                  const Row(
+                    children: [
+                      // Card login untuk facebook
+                      CardLoginSocial(
+                        logo: 'assets/icons/facebook.svg',
+                        title: 'Facebook',
+                      ),
+
+                      SizedBox(width: 15),
+
+                      // Card login untuk google
+                      CardLoginSocial(
+                        logo: 'assets/icons/google.svg',
+                        title: 'Google',
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 110),
+
+                  RichText(
+                    text: TextSpan(
+                      text: 'Belum punya akun? ',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        color: secondary,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Daftar',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, color: primary),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
--- lib/register.dart
+++ lib/register.dart
@@ -11,116 +11,118 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 70, left: 33, right: 33),
-          child: Column(
-            children: [
-              // Mini icon
-              SvgPicture.asset('assets/icons/mini_icons.svg'),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 70, left: 33, right: 33),
+            child: Column(
+              children: [
+                // Mini icon
+                SvgPicture.asset('assets/icons/mini_icons.svg'),
 
-              const SizedBox(height: 36),
+                const SizedBox(height: 36),
 
-              // Heading pembuka
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 48),
-                child: Text(
-                  'Daftarkan diri terlebih dahulu',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                    color: secondary,
+                // Heading pembuka
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 48),
+                  child: Text(
+                    'Daftarkan diri terlebih dahulu',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                      color: secondary,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              Text(
-                'Kami menjamin keamanan dan privasi anda',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  color: neutral,
-                  fontSize: 12,
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // Form pendaftaran
-              const Column(
-                children: [
-                  // Username
-                  MyTextField(
-                    hintText: 'Username',
-                    title: 'Username',
-                    myIcons: 'assets/icons/user.svg',
-                    hideText: false,
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // Email
-                  MyTextField(
-                    hintText: 'Email',
-                    title: 'Email',
-                    myIcons: 'assets/icons/mail.svg',
-                    hideText: false,
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // Password
-                  MyTextField(
-                    hintText: 'Password',
-                    title: 'Password',
-                    myIcons: 'assets/icons/key.svg',
-                    hideText: true,
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // Telepon
-                  MyTextField(
-                    hintText: 'Telepon',
-                    title: 'Telepon',
-                    myIcons: 'assets/icons/flag_id.svg',
-                    hideText: false,
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 50),
-
-              // Button daftar
-              MyButton(
-                title: 'Daftar Sekarang',
-                color: primary,
-              ),
-
-              const SizedBox(height: 18),
-
-              // Action jika sudah daftar
-              RichText(
-                text: TextSpan(
-                  text: 'Sudah daftar? ',
+                Text(
+                  'Kami menjamin keamanan dan privasi anda',
                   style: TextStyle(
                     fontFamily: 'Poppins',
-                    fontSize: 14,
-                    color: secondary,
+                    color: neutral,
+                    fontSize: 12,
                   ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Login',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600, color: primary),
+                ),
+
+                const SizedBox(height: 50),
+
+                // Form pendaftaran
+                const Column(
+                  children: [
+                    // Username
+                    MyTextField(
+                      hintText: 'Username',
+                      title: 'Username',
+                      myIcons: 'assets/icons/user.svg',
+                      hideText: false,
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // Email
+                    MyTextField(
+                      hintText: 'Email',
+                      title: 'Email',
+                      myIcons: 'assets/icons/mail.svg',
+                      hideText: false,
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // Password
+                    MyTextField(
+                      hintText: 'Password',
+                      title: 'Password',
+                      myIcons: 'assets/icons/key.svg',
+                      hideText: true,
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // Telepon
+                    MyTextField(
+                      hintText: 'Telepon',
+                      title: 'Telepon',
+                      myIcons: 'assets/icons/flag_id.svg',
+                      hideText: false,
                     ),
                   ],
                 ),
-              )
-            ],
+
+                const SizedBox(height: 50),
+
+                // Button daftar
+                MyButton(
+                  title: 'Daftar Sekarang',
+                  color: primary,
+                ),
+
+                const SizedBox(height: 18),
+
+                // Action jika sudah daftar
+                RichText(
+                  text: TextSpan(
+                    text: 'Sudah daftar? ',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      color: secondary,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, color: primary),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
